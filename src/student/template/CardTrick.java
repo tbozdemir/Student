8ce5ca1559c67ebadeab828e02a7400292797174
tbozdemir <50675934+tbozdemir@users.noreply.github.com>
2019-05-23 10:56:04 -0400

    Update CardTrick.java

--- src/student/template/CardTrick.java
+++ src/student/template/CardTrick.java
@@ -16,6 +16,7 @@ public class CardTrick {
      public static void main(String[] args)
     {
         Card[] magicHand = new Card[7];
+         Card luckyCard=new Card();
         
         for (int i=0; i<magicHand.length; i++)
         {
